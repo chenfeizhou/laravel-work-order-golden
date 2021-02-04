45c3b54aba7406fc3ab3d2ea7e2d585ab1a21232
chenfeizhou <1114526565@qq.com>
2021-02-04 14:26:28 +0800

    <fix>: router

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Routing\Router;
 
 Route::group([
     'prefix'        => config('golden-work-order.route.prefix'),
-    'middleware'    => config('golden-work-order.rout.middleware'),
+    'middleware'    => config('golden-work-order.route.middleware'),
     'namespace'     => 'Chenfeizhou\WorkOrder\Controllers',
 ], function (Router $router) {
     // 工单回调地址
