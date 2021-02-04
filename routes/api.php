72c2c6ca26dc9499359947ae46e82e9431927bc1
chenfeizhou <1114526565@qq.com>
2021-02-04 14:20:53 +0800

    <fix>: router

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,12 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use Illuminate\Routing\Router;
 
 Route::group([
     'prefix'        => config('golden-work-order.route.prefix'),
     'middleware'    => config('golden-work-order.rout.middleware'),
     'namespace'     => 'Chenfeizhou\WorkOrder\Controllers',
-], function (Route $router) {
+], function (Router $router) {
     // 工单回调地址
     $router->post('/notify/golden-work-order/callback', 'WorkOrderController@auditCallback');
 });
